f1e9ea400a05eba4b88290acfda1cbf1178b9862
Alejandrosbk <alejo99sbk@gmail.com>
2023-07-26 23:08:31 -0500

    actualizacion de datos, estilos y servicio

--- src/app/interfaces/personal-information.ts
+++ src/app/interfaces/personal-information.ts
@@ -1,10 +1,12 @@
 export interface PersonalInfo {
-  position?: string;
-  description?: string;
-  address?: string;
-  email?: string;
-  name?: string;
-  phone?: string;
-  date?: string;
-  interest?: string;
+  position?: string,
+  description?: string,
+  address?: string,
+  email?: string,
+  name?: string,
+  phone?: string,
+  date?: string,
+  interest?: string,
+  actualPosition?: string,
+  actualCompany?: string
 }
--- src/app/services/data-firebase.service.ts
+++ src/app/services/data-firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PersonalInfo } from '../interfaces/personal-information';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -24,50 +25,74 @@ export class DataFirebaseService {
   }
 
   private personalInfo() {
-    this.http.get('assets/data/personal-information.json')
-    .subscribe((resp: PersonalInfo) => {
-      this.info = resp;
-      console.table(resp);
+    this.http.get('assets/data/personal-information.json').subscribe({
+      next: (resp: PersonalInfo) => {
+        this.info = resp;
+        // console.log(resp);
+      },
+      error: (error) => {
+        console.log(error);
+      }
     });
   }
 
-  private CargarProfesional(){
-    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Profesional.json')
-      .subscribe((resp: any) => {
+  private CargarProfesional() {
+    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Profesional.json').subscribe({
+      next: (resp: any) => {
         this.profesional = resp;
         // console.log(resp);
+      },
+      error: (error) => {
+        console.log(error);
+      }
     });
   }
 
   private CargarEducacion(){
-    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Educacion.json')
-      .subscribe((resp: any) => {
+    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Educacion.json').subscribe({
+      next: (resp: any) => {
         this.educacion = resp;
         // console.log(resp);
+      },
+      error: (error) => {
+        console.log(error);
+      }
     });
   }
 
   private CargarTestimonio(){
-    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Testimonios.json')
-      .subscribe((resp: any) => {
+    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Testimonios.json').subscribe({
+      next: (resp: any) => {
         this.testimonio = resp;
         // console.log(resp);
+      },
+      error: (error) => {
+        console.log(error);
+      }
     });
   }
 
   private CargarExperiencia(){
-    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Experiencia.json')
-      .subscribe((resp: any) => {
+    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Experiencia.json').subscribe({
+      next: (resp: any) => {
         this.experiencia = resp;
         // console.log(resp);
+      },
+      error: (error) => {
+        console.log(error);
+      }
     });
   }
 
   private CargarServicios(){
-    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Servicios.json')
-      .subscribe((resp: any) => {
+    this.http.get('https://cv-alejandroc-default-rtdb.firebaseio.com/Servicios.json').subscribe({
+      next: (resp: any) => {
         this.servicios = resp;
         // console.log(resp);
+      }, 
+      error: (error) => {
+        console.log(error);
+      }
     });
   }
 }
